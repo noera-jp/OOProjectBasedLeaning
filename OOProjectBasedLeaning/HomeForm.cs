db8847db03e332f436d0dddf78b8a9b7cde6da5a
奏人 今川 <C3031@oic.jp>
2025-06-13 11:26:00 +0900

    homeFormをドラッグアンドドロップに対応

--- OOProjectBasedLeaning/HomeForm.cs
+++ OOProjectBasedLeaning/HomeForm.cs
@@ -1,13 +1,33 @@
 namespace OOProjectBasedLeaning
 {
 
-    public partial class HomeForm : Form
+    public partial class HomeForm : DragDropForm
     {
 
         public HomeForm()
         {
 
             InitializeComponent();
+
+        }
+
+
+        protected override void OnFormDragEnterSerializable(DragEventArgs dragEventArgs)
+        {
+
+            dragEventArgs.Effect = DragDropEffects.Move;
+
+        }
+
+        protected override void OnFormDragDropSerializable(object? serializableObject, DragEventArgs dragEventArgs)
+        {
+
+            if (serializableObject is DragDropPanel)
+            {
+
+                (serializableObject as DragDropPanel).AddDragDropForm(this, PointToClient(new Point(dragEventArgs.X, dragEventArgs.Y)));
+
+            }
 
         }
 
