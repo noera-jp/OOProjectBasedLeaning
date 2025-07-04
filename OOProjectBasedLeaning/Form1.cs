26df0ad654c2019bc896981aac5eeee5fe39ce50
himer <himer@RURIHIME>
2025-07-04 10:20:35 +0900

    毎秒間隔更新の現在時刻の表示

--- OOProjectBasedLeaning/Form1.cs
+++ OOProjectBasedLeaning/Form1.cs
@@ -18,8 +18,15 @@ namespace OOProjectBasedLeaning
             // ‰ïŽÐ
             new CompanyForm().Show();
 
+            timer1.Enabled = true;
         }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime now=DateTime.Now;
+            label1.Text = now.ToString("yyyy/MM/dd HH:mm:ss");
+            timer1.Interval = 1000;
+        }
     }
 
 }
