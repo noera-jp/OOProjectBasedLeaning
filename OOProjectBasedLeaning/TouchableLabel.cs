2454848f980d0a325163208c5107ab3548b46a3d
奏人 今川 <C3031@oic.jp>
2025-07-04 10:43:03 +0900

    classの追加

---
+++ OOProjectBasedLeaning/TouchableLabel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOProjectBasedLeaning
+{
+    public abstract class TouchableLabel : Label
+    {
+        public TouchableLabel()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void InitializeComponent()
+        {
+
+            Click += touchLabel_Click;
+
+        }
+
+        private void touchLabel_Click(object? sender, EventArgs e)
+        {
+
+            OnTouch();
+
+        }
+
+        protected abstract void OnTouch();
+
+    }
+
+    public class NullTouchableLabel : TouchableLabel
+    {
+
+        private static readonly TouchableLabel instance = new NullTouchableLabel();
+
+        private NullTouchableLabel()
+        {
+
+        }
+
+        public static TouchableLabel Instance
+        {
+
+            get { return instance; }
+
+        }
+
+        protected override void OnTouch()
+        {
+
+        }
+
+    }
+}
