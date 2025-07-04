225f5999bbebf0217ce6f410d82445ba7234b596
高木　日向 <C3158@oic.jp>
2025-07-04 10:41:03 +0900

    Edittable.cs、EditMode.cs の追加

---
+++ OOProjectBasedLeaning/Edittable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOProjectBasedLeaning
+{
+    public interface Edittable
+    {
+        void ChangeDisplayMode();
+
+        void ChangeEditMode();
+    }
+}
