2a62d54dcb9879a803d2d82f2111b92bf05e5b80
高木　日向 <C3158@oic.jp>
2025-07-11 10:14:50 +0900

    EmployeeText.csの作成

---
+++ OOProjectBasedLeaning/EmployeeTextBox.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOProjectBasedLeaning
+{
+    public class EmployeeTextBox : TextBox
+    {
+        private Employee employee = NullEmployee.Instance;
+
+        public EmployeeTextBox(Employee employee)
+        {
+            this.employee = employee;
+
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AutoSize = true;
+            Text = employee.Name;
+        }
+
+        public void Save()
+        {
+            employee.Name = Text;
+        }
+    }
+
+    public class NullEmployeeNameTextBox : EmployeeTextBox
+    {
+        private static readonly EmployeeTextBox instance = new NullEmployeeNameTextBox();
+
+        private NullEmployeeNameTextBox() : base(NullEmployee.Instance)
+        {
+            //this constructor is used for the null object pattem
+        }
+        public static EmployeeTextBox Instance { get { return instance; } }
+
+        public override string Text
+        {
+            get { return string.Empty; }
+            set { /*Do nothing */}//Prevent setting a name for the null object
+
+        }
+    }
+}
